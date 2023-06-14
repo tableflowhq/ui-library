829c39942d3700f908c403f53309df1e6380566c
ciminelli <eric@inquery.io>
2023-06-14 11:36:46 -0700

    Updated timestampDisplay to take in seconds or milliseconds, updated date format

--- src/utils/timestampDisplay.ts
+++ src/utils/timestampDisplay.ts
@@ -1,14 +1,19 @@
 const timestampDisplay = (timestamp: number) => {
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-  const date = new Date(timestamp);
-  const month = months[date.getMonth()];
-  const day = date.getDate();
-  const year = date.getFullYear();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-
-  return `${month} ${day}, ${year} ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+    // Loosely check if the timestamp is seconds or milliseconds by seeing which is closer to now
+    const time = new Date(timestamp).getTime();
+    const now = Date.now();
+    let adjustedTimestamp = timestamp;
+    if (Math.abs(now - time) > Math.abs(now - time * 1000)) {
+        adjustedTimestamp = adjustedTimestamp * 1000;
+    }
+    const date = new Date(adjustedTimestamp);
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hoursMinutes = new Intl.DateTimeFormat("default", { hour12: true, hour: "numeric", minute: "numeric" }).format(date);
+    return `${month} ${day}, ${year} ${hoursMinutes}`;
 };
 
 export default timestampDisplay;
