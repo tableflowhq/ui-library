d8a1a8a93e6ea8845990c0be3900459d74dea211
ciminelli <eric@inquery.io>
2023-06-15 10:45:38 -0700

    Updated svgs and package names

--- src/Tableflow/index.tsx
+++ src/Tableflow/index.tsx
@@ -1,9 +1,8 @@
-import useThemeStore from "../Stores/useThemeStore";
 import classes from "../utils/classes";
 import { TableflowProps } from "./types";
 import style from "./style/Tableflow.module.scss";
-import { ReactComponent as TableflowLogoColor } from "../assets/tableflow-logo-color.svg";
-import { ReactComponent as TableflowLogo } from "../assets/tableflow-logo.svg";
+import { ReactComponent as TableflowLogo } from "../assets/tableflow-logo-color.svg";
+import { ReactComponent as TableflowLogoColor } from "../assets/tableflow-logo.svg";
 
 export default function Tableflow({ color, size = "normal" }: TableflowProps) {
     const className = classes(["tableflow", style.tableflow, style[size]]);
