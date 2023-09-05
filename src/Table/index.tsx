cc8fd492cc2b0cad947e9f5e16d8463ea4015f57
Carlos Mercado <carlos.mercado.pc@gmail.com>
2023-09-05 16:07:00 -0500

    Renamed vertical to horizontal

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -74,7 +74,7 @@ export default function Table({
             <div className={tableStyle} role="table">
                 {headingContent}
 
-                <div className={classes([style.tbody, hasScrollX && style.tVerticalScroll])} role="rowgroup"> 
+                <div className={classes([style.tbody, hasScrollX && style.horizontalScroll])} role="rowgroup">
                     {data.map((d, i) => {
                         const key = keyAsId && d?.[keyAsId] ? d[keyAsId] : i;
                         const props = { datum: d, onClick: onRowClick };
